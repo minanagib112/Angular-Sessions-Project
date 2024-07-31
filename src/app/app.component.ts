c06bbee4b2dcee643757bacaf37e7153ddd926b0
Mina Naguib <minanagib628@gmail.com>
2024-08-01 01:10:00 +0300

    Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import HomeComponent from './components/home/home.component';
 import AboutComponent from './components/About/about.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,AboutComponent],
+  imports: [RouterOutlet,HomeComponent,AboutComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app1';
+  userName: string = 'Mina';
+  userAge: number = 25;
+
+  changeData() {
+    this.userName = 'Menna';
+    this.userAge = 23;
+  }
+  changeInput(e:any){
+    this.userName = e.target.value
+  }
 }
